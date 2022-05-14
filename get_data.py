2fc3f17e60522d64a37649e38b6cb2f09fce393d
Aristeu Roriz <aristeuroriz@gmail.com>
2022-05-14 00:49:42 -0300

    script

--- get_data.py
+++ get_data.py
@@ -1,6 +1,14 @@
+from distutils.command.build_scripts import first_line_re
 import sgs
 import pyodbc 
 from datetime import datetime
+import math
+import pandas as pd
+
+import sys
+
+start = sys.argv[1]
+end = sys.argv[2]
 
 server = 'tcp:localhost' 
 database = 'sucri' 
@@ -11,23 +19,75 @@ cursor = cnxn.cursor()
     
 format_date = "%Y/%m/%d %H:%M:%S"
 
-# CDI = 12
-# INCC = 192  #  National Index of Building Costs
-# df = sgs.dataframe([CDI, INCC], start='02/01/2018', end='31/12/2018')
+def get_series_metadata():
+    codes_response = []
+    results = []
+    series_fail = []
+    print("Searching range: ", start, end)
+    for serie in range(int(start), int(end), 1):
+        results = sgs.search_ts(serie, language = "pt")
+        print('######################################################')
+        print('Searching code: {}'.format(serie))
+        if results:
+            print('------------------------------------------------------')
+            codes_response.append(serie)
+            print(serie)
+            print(results[0]["frequency"])
+            # if results[0]["frequency"] == 'M':
+            code = results[0]["code"]
+            name = str(results[0]["name"])
+            unit = str(results[0]["unit"])
+            frequency = str(results[0]["frequency"])
+            first_value = str(results[0]["first_value"])
+            last_value = str(results[0]["last_value"]) if not str(results[0]["last_value"]) == '-' else ''
+            source = str(results[0]["source"])
+            query = "INSERT INTO SGS.series_meta (code, name, unit, frequency, first_value, last_value, source) VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}');".format(
+                code, name, unit, frequency, first_value , last_value, source)
+            print(query)
+            cursor.execute(query)
+            record_id = cursor.execute('SELECT @@IDENTITY AS id;').fetchone()[0]
+            print("record_id: ", record_id)
+            cnxn.commit()  
+            get_series_data(record_id, results[0]["code"], series_fail)
+    print('######################################################')
+    f = open("codes_found-{}.txt".format(datetime.now()), "w")
+    f.write(str(codes_response))
+    f.close()
+    print(codes_response)
+    f = open("series_fail-{}.txt".format(datetime.now()), "w")
+    f.write(str(series_fail))
+    f.close()
+    print(series_fail)
+    print('######################################################')
+            
+            
+def get_series_data(record_id, code, series_fail):
+    print(record_id, code)
+    try:
+        df = sgs.dataframe([code], start='02/01/2017', end='31/12/2030')
+    except:
+        series_fail.append(code)
+        return
+    for row in df.iterrows():
+        value_number = float(row[1]) if not math.isnan(float(row[1])) else -1
+        value_date = str(row[0])
+        print(code, value_number, value_date, record_id)
+        query = "INSERT INTO SGS.series_data (code, value_number, value_date, meta_id) VALUES ({}, {}, '{}', '{}');".format(
+                code, value_number, value_date, record_id)
+        print(query)
+        cursor.execute(query) 
+        cnxn.commit()
 
-# print(sgs.metadata(df))
-# print('-----------------------')
-# print(df.head(20))
 
-codigos = []
 
-for x in range(50):
-  CDI_CODE = x + 1
-  results = sgs.search_ts(CDI_CODE, language = "pt")
-  if results:
-      query = "INSERT INTO SGS.series_meta (code, name, unit, frequency, first_value, last_value, source) VALUES ({}, '{}', '{}', '{}', '{}', '{}', '{}');".format(
-          results[0]["code"], str(results[0]["name"]), str(results[0]["unit"]), str(results[0]["frequency"]), 
-         str(results[0]["first_value"]) , str(results[0]["last_value"]), str(results[0]["source"]))
-      print(query)
-      cursor.execute(query) 
-      cnxn.commit()
+def set_inatives():
+        query = "EXEC SGS.get_inactives;"
+        cursor.execute(query) 
+        for row in cursor.fetchall():
+            query = "UPDATE SGS.series_meta SET active = 0 WHERE code = {};".format(row[0])
+            print(query)
+            cursor.execute(query) 
+            cnxn.commit()
+
+get_series_metadata()
+set_inatives()
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from flask import Flask
 from flask import jsonify
+import pyodbc 
+from datetime import datetime
+import math
+
+server = 'tcp:localhost' 
+database = 'sucri' 
+username = 'sa' 
+password = 'mssql1Ipw' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
 
 app = Flask(__name__)
 app.run(debug=True)
@@ -10,5 +21,86 @@ def hello_world():
 
 
 @app.route('/sgs/inactives')
-def sgs_inatives():
-    return jsonify(test = 'testing')
+def sgs_inactives():
+    try:
+        data = []
+        query = "SELECT * FROM SGS.series_meta sm WHERE sm.active = 0;"
+        print(query)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        print(columns)
+        for row in rows:
+            obj = {
+                columns[0]: row[0],
+                columns[1]: row[1],
+                columns[2]: row[2],
+                columns[3]: row[3],
+                columns[4]: row[4],
+                columns[5]: row[5],
+                columns[6]: row[6],
+                columns[7]: row[7],
+                columns[8]: row[8]
+                }
+            data.append(obj)
+        return jsonify(data)
+    except:
+        print("exception on get inactives")
+        return jsonify(Exception = "exception on get inactives")
+
+@app.route('/sgs/actives')
+def sgs_actives():
+    try:
+        data = []
+        query = "SELECT * FROM SGS.series_meta sm WHERE sm.active = 1;"
+        print(query)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        print(columns)
+        for row in rows:
+            obj = {
+                columns[0]: row[0],
+                columns[1]: row[1],
+                columns[2]: row[2],
+                columns[3]: row[3],
+                columns[4]: row[4],
+                columns[5]: row[5],
+                columns[6]: row[6],
+                columns[7]: row[7],
+                columns[8]: row[8]
+                }
+            data.append(obj)
+        return jsonify(data)
+    except:
+        print("exception on get inactives")
+        return jsonify(Exception = "exception on get inactives")
+
+@app.route('/sgs/get_all_metadata')
+def sgs_get_all_metadata():
+    try:
+        data = []
+        query = "SELECT * FROM SGS.series_meta;"
+        print(query)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        print(columns)
+        for row in rows:
+            obj = {
+                columns[0]: row[0],
+                columns[1]: row[1],
+                columns[2]: row[2],
+                columns[3]: row[3],
+                columns[4]: row[4],
+                columns[5]: row[5],
+                columns[6]: row[6],
+                columns[7]: row[7],
+                columns[8]: row[8]
+                }
+            data.append(obj)
+        return jsonify(data)
+    except:
+        print("exception on get inactives")
+        return jsonify(Exception = "exception on get all metadata")
+
